4867fc7c0d13dec31a38fcb3c8a6651dc4de4a02
dprajapat9998 <94288455+dprajapat9998@users.noreply.github.com>
2026-09-23 23:41:26 +0530

    Time: 70 ms (7.19%), Space: 166.6 MB (5.01%) - LeetHub

--- 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.java
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.java
@@ -1,24 +1,25 @@
 class Solution {
     public int minOperations(int[] nums, int x) {
-        int n = nums.length;
-        int sum = 0;
-        HashMap<Integer, Integer> mp = new HashMap<>();
-        mp.put(0, -1);
-        for (int i = 0; i < n; i++) {
-            sum += nums[i];
-            mp.put(sum, i);
+        int sum=0;
+        HashMap<Integer,Integer> map = new HashMap<>();
+        map.put(0,-1);
+        for(int i=0;i<nums.length;i++){
+            sum+=nums[i];
+            map.put(sum,i);
         }
-        if (sum < x)
-            return -1;
-        int restSum = sum - x;
-        int longest = Integer.MIN_VALUE;
-        sum = 0;
-        for (int i = 0; i < n; i++) {
-            sum += nums[i];
-            if (mp.containsKey(sum - restSum)) {
-                longest = Math.max(longest, i - mp.get(sum - restSum));
+        if(sum<x) return -1;
+
+        int ans =Integer.MIN_VALUE;
+        
+        int target = sum-x;
+         sum=0;
+        for(int i=0;i<nums.length;i++){
+             sum+=nums[i];
+            int value = sum-target;
+            if(map.containsKey(value)){
+                ans=Math.max(ans,i-map.get(value));
             }
         }
-        return longest == Integer.MIN_VALUE ? -1 : n - longest;
+        return ans==Integer.MIN_VALUE ? -1 : nums.length-ans; 
     }
 }
